757dd0b575184a528fd169c720255b3fe7bbc365
dlgurwls0529 <dlgurwls0529@gmail.com>
2022-07-06 04:20:57 +0900

    Initial commit

---
+++ src/BasicSingleton.java
@@ -0,0 +1,14 @@
+public class BasicSingleton {
+
+    private static BasicSingleton instance;
+
+    private BasicSingleton() { };
+
+    public static BasicSingleton getInstance() {
+        if(instance == null) {
+            instance = new BasicSingleton();
+        }
+
+        return instance;
+    }
+}
---
+++ src/SynchronizedSingleton.java
@@ -0,0 +1,14 @@
+public class SynchronizedSingleton {
+
+    private static SynchronizedSingleton instance;
+
+    private SynchronizedSingleton() { };
+
+    public static synchronized SynchronizedSingleton getInstance() {
+        if(instance == null) {
+            instance = new SynchronizedSingleton();
+        }
+
+        return instance;
+    }
+}
